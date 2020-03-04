bda51a977423b3c455ca68d7506f5919865f1537
Josh Tukman <josh.tukman@gmail.com>
2020-03-04 09:23:59 -0700

    Test Artist attributes

--- test/artist_test.rb
+++ test/artist_test.rb
@@ -9,4 +9,13 @@ class ArtistTest < Minitest::Test
     artist_1 = Artist.new({id: "2", name: "Ansel Adams", born: "1902", died: "1984", country: "United States"})
     assert_instance_of Artist, artist_1
   end
+
+  def test_it_has_attributes
+    artist_1 = Artist.new({id: "2", name: "Ansel Adams", born: "1902", died: "1984", country: "United States"})
+    assert_equal "2", artist_1.id
+    assert_equal "Ansel Adams", artist_1.name
+    assert_equal "1902", artist_1.born
+    assert_equal "1984", artist_1.died
+    assert_equal "United States", artist_1.country
+  end
 end
