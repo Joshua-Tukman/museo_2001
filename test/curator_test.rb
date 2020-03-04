1e56e297d7ad92e83dbfb248e626f23b9f800f8a
Josh Tukman <josh.tukman@gmail.com>
2020-03-04 09:38:27 -0700

    Add ability to add photos to curator collection

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -15,6 +15,15 @@ class CuratorTest < Minitest::Test
     curator = Curator.new
     assert_equal [], curator.photographs
   end
+
+  def test_it_can_add_photos
+    curator = Curator.new
+    photo_1 = Photograph.new({id: "1", name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: "1", year: "1954"})
+    photo_2 = Photograph.new({id: "2", name: "Moonrise, Hernandez", artist_id: "2", year: "1941"})
+    curator.add_photograph(photo_1)
+    curator.add_photograph(photo_2)
+    assert_equal [photo_1, photo_2], curator.photographs
+  end
 end
 # ```markdown
 # There are **3** points possible in Iteration 2:
@@ -24,27 +33,20 @@ end
 # 6. Curator #find_artist_by_id
 # ```
 #
-# Use TDD to create a `Curator` class that responds to the following interaction pattern:
 
-# pry(main)> curator = Curator.new
-# # => #<Curator:0x00007fb3d9111cc0...>
-#
-# pry(main)> curator.photographs
-# # => []
-#
-# pry(main)> photo_1 = Photograph.new({
-#      id: "1",
-#      name: "Rue Mouffetard, Paris (Boy with Bottles)",
-#      artist_id: "1",
-#      year: "1954"
+# photo_1 = Photograph.new({
+# id: "1",
+# name: "Rue Mouffetard, Paris (Boy with Bottles)",
+# artist_id: "1",
+# year: "1954"
 # })
 # # => #<Photograph:0x00007fb3d8ea1cd8...>
 #
-# pry(main)> photo_2 = Photograph.new({
-#      id: "2",
-#      name: "Moonrise, Hernandez",
-#      artist_id: "2",
-#      year: "1941"
+# photo_2 = Photograph.new({
+# id: "2",
+# name: "Moonrise, Hernandez",
+# artist_id: "2",
+# year: "1941"
 # })
 # # => #<Photograph:0x00007fb3d8e0bfa8...>
 #
